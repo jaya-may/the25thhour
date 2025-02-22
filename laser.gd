67526b7cf51d821baa7d6a3fb70295850901f6b9
Nebula <furnissj@my.erau.edu>
2025-02-22 15:06:00 -0500

    finished laser

--- laser.gd
+++ laser.gd
@@ -1,19 +1,20 @@
 extends Area2D
 
 @export var damage: float = -10.0  # Negative means it reduces HP
-var timer = 1
+var timer = 1.0
 var killmode = false
 
-func _ready():
-	connect("body_entered", _on_body_entered)  # Connect signal
-
 func _process(delta: float) -> void:
-	timer-=delta
-	if(timer<=.5):
-		killmode=true
+	timer -= delta
+	
+	if timer <= 0.5 and not killmode:
+		killmode = true
+		_check_for_players()  # ONLY check for players now
 
-func _on_body_entered(body):
-	if(killmode):
-		if body.is_in_group("Player"):
-			if body.has_method("modifyHp"): 
-				body.modifyHp(damage)  
+	if timer <= 0:
+		queue_free()
+
+func _check_for_players():
+	for body in get_overlapping_bodies():
+		if body.is_in_group("Player") and body.has_method("modifyHp"):
+			body.modifyHp(damage)  # Apply damage only at this moment
